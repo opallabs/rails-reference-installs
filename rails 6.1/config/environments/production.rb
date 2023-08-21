50a0552197c4b1f0fc0af5f5e39606752cdfeb67
Jordan Cole <33663+stilist@users.noreply.github.com>
2023-08-21 12:42:52 -0700

    Regenerate all versions

--- rails 6.1/config/environments/production.rb
+++ rails 6.1/config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "r61_production"
+  # config.active_job.queue_name_prefix = "rails_6_1_production"
 
   config.action_mailer.perform_caching = false
 
